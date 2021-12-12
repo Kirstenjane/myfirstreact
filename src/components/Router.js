6b1f40e84b2752001a2731f55fa82772b6bba765
Kirstenjane <90952383+Kirstenjane@users.noreply.github.com>
2021-12-12 23:37:49 +0800

    initial commit
    initial commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -12,7 +12,7 @@ const Router = () => {
                 <BrowserRouter>
                 
                     <Routes>
-                        <Route exact path='/myfirstreact' element={ <Home/>}/>
+                        <Route exact path='/myfirstreact/' element={ <Home/>}/>
                         <Route exact path='/myfirstreact/employees' element={<Employee/>}/>
                         <Route exact path='/myfirstreact/add' element={<AddEmployee/>}/>
                         <Route exact path='/myfirstreact/edit/:employeeId' element={<AddEmployee/>}/>
