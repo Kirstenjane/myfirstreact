f19dcfe270041e1df8df5642bb72adabf3564375
Kirstenjane <90952383+Kirstenjane@users.noreply.github.com>
2021-12-15 08:41:26 +0800

    yes
    yes

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 export default axios.create({
-        baseURL: 'http://localhost:8080/api/v1',
+        baseURL: 'https://anuma-myfirstreact.herokuapp.com/SpringJava',
         headers:{
             'Content-Type': 'application/json'
         }     
