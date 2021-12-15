ea4df4e0ad6b17ca38270f3a9c8eb1823f5f8ce4
Kirstenjane <90952383+Kirstenjane@users.noreply.github.com>
2021-12-15 10:14:56 +0800

    fullstack
    fullstack

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 export default axios.create({
-        baseURL: 'https://anuma-myfirstreact.herokuapp.com/api/v1/employees',
+        baseURL: 'https://anuma-myfirstreact.herokuapp.com/api/v1',
         headers:{
             'Content-Type': 'application/json'
         }     
